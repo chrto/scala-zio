5b50ab27642e9b6df9b86a1abf5033efc5139b20
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2025-03-08 23:44:28 +0100

    added tests - Main

--- src/test/scala/zio2demo/MainSpec.scala
+++ src/test/scala/zio2demo/MainSpec.scala
@@ -4,7 +4,5 @@ import zio._
 import zio.test._
 
 object MainSpec extends ZIOSpecDefault {
-  def spec: Spec[
-    zio2demo.MainSpec.Environment & (zio.test.TestEnvironment & zio.Scope), Any
-      ] = ???
+  def spec: Spec[Environment & (TestEnvironment & Scope), Any] = test("...")(assertTrue(true))
 }
